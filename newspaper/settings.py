d530b19d9a56a64426ebd7b9b0379059a7b39928
Ubuntu <ubuntu@ip-172-31-83-56.ec2.internal>
2019-08-26 12:44:03 +0000

    Heroku host added to allowed hosts

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['4fcfc9252af348e7ad3f0ce02d7a2cf5.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['4fcfc9252af348e7ad3f0ce02d7a2cf5.vfs.cloud9.us-east-1.amazonaws.com', 'paraic-fullstack-project.herokuapp.com']
 
 
 # Application definition
